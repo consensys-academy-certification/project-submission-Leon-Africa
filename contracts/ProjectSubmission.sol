e1affaf07ffd1d538709ae50fa5747b82c33b99a
Leon Africa <mr.leon.africa@gmail.com>
2019-12-26 21:10:11 +0200

    Step 1: #2 #3 #4 #5 #6 (#28)
    Step 1 complete

--- contracts/ProjectSubmission.sol
+++ contracts/ProjectSubmission.sol
@@ -1,18 +1,31 @@
-pragma... // Step 1
+pragma solidity ^0.5.0;
 
-contract ProjectSubmission { // Step 1
+contract ProjectSubmission {
 
-    ...owner... // Step 1 (state variable)
+  address payable public owner = msg.sender;
     // ...ownerBalance... // Step 4 (state variable)
-    modifier onlyOwner() { // Step 1
-      ...
-    }
+
+    event registration_message(string message);
+    string message_success = "Succesfully Registred!";
+     //error message is specifically abstract to avoid showing account info
+    string message_failed = "Failed Registration - please contact info@universityregitration.org";
     
-    struct University { // Step 1
-        ...available...
-        ...balance...
+    event avaliable_status_message(string message);
+    string message_unavaliable = "Succesfully changed univeristy status to unavaliable!";
+    string message_immutable = "Cannot undo immutable action of set to unavaliable - please contact info@universityregitration.org";
+   
+
+    modifier onlyOwner(){
+      require(msg.sender == owner, "You are not the owner of this contract!");
+      _;
     }
-    ...universities... // Step 1 (state variable)
+      
+    struct University { // Step 1
+        bool available;
+        bool isregistered;
+        uint balance;
+    }
+    mapping(address => University) public universities;
     
     // enum ProjectStatus { ... } // Step 2
     // struct Project { // Step 2
@@ -22,13 +35,40 @@ contract ProjectSubmission { // Step 1
     //     ...balance...
     // }
     // ...projects... // Step 2 (state variable)
+
     
-    function registerUniversity... { // Step 1
-      ...
+    function registerUniversity(address university_address) public onlyOwner{ // Step 1
+     
+     //A university that is already registred cannot register again
+     if (universities[university_address].isregistered == true){
+        emit registration_message(message_failed);
+     }
+     //If the university is not yet registered then register it. Booleans are false by default
+     else if (universities[university_address].isregistered == false){
+          University memory university = University(true, true, 0);
+          universities[university_address] = university;
+          emit registration_message(message_success);
+
+     }else {
+       emit registration_message(message_failed);
+     }
+     
+
     }
     
-    function disableUniversity... { // Step 1
-      ...
+    function disableUniversity(address university_address) public onlyOwner {
+      
+      //https://github.com/consensys-academy-certification/project-submission-Leon-Africa/issues/6
+      //Action performed by disableUniversity() cannot be undone
+      if(universities[university_address].available = false){
+        emit avaliable_status_message(message_immutable);
+      }else{
+         universities[university_address].available = false;
+         emit avaliable_status_message(message_unavaliable);
+      }
+      
+      
+
     }
     
     // function submitProject... { // Step 2 and 4
