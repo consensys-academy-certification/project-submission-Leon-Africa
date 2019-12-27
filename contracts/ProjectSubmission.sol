7be24cf253fee20ce8cd102197d4f27ec62d695f
Leon-Africa <mr.leon.africa@gmail.com>
2019-12-27 15:10:28 +0200

    updates

--- contracts/ProjectSubmission.sol
+++ contracts/ProjectSubmission.sol
@@ -6,20 +6,10 @@ contract ProjectSubmission {
   uint public ownerBalance;
 
     event registration_message(string message);
-    string message_success = "Succesfully Registred!";
-     //error message is specifically abstract to avoid showing account info
-    string message_failed = "Failed Registration - please contact info@universityregitration.org";
-    
     event avaliable_status_message(string message);
-    string message_unavaliable = "Succesfully changed univeristy status to unavaliable!";
-    string message_immutable = "Cannot undo immutable action of set to unavaliable - please contact info@universityregitration.org";
-   
     event submit_project_message(string message);
-    string submit_project_success = "Project Successfully submitted!";
-
     event submit_review_message(string message);
-    string submit_review_success = "Project Review Status succefuly updated!";
-    string submit_review_error = "Error: Review status must either be Rejected or Approved.";
+    
 
     modifier onlyOwner(){
       require(msg.sender == owner, "You are not the owner of this contract!");
@@ -39,6 +29,7 @@ contract ProjectSubmission {
          address payable author;
          address payable university;
          ProjectStatus status;
+         bool issubmitted;
          uint balance;
      }
     
@@ -47,36 +38,39 @@ contract ProjectSubmission {
     
     function registerUniversity(address university_address) public onlyOwner{ // Step 1
      
-     //A university that is already registred cannot register again
-     if (universities[university_address].isregistered == true){
-        emit registration_message(message_failed);
+    //A university that is already registred cannot register again
+     if (universities[university_address].isregistered){
+         
+         if(!universities[university_address].available){
+           //Has been disabled for project submission
+           revert("Cannot undo immutable action of setting University to unavaliable");
+         }
+         
+         //Already registred
+         revert("This University has already been registred.");
+        
      }
-     //If the university is not yet registered then register it. Booleans are false by default
-     else if (universities[university_address].isregistered == false){
+     //If the university is not yet registered then register it. Booleans are false by default so an unregistred university cannot be avaliable
+     else if (!universities[university_address].isregistered){
           University memory university = University(true, true, 0);
           universities[university_address] = university;
-          emit registration_message(message_success);
+          emit registration_message("Succesfully Registred!");
 
      }else {
-       emit registration_message(message_failed);
+         revert("Failed Registration - please contact info@universityregitration.org");
      }
      
 
     }
     
     function disableUniversity(address university_address) public onlyOwner {
-      
-      //https://github.com/consensys-academy-certification/project-submission-Leon-Africa/issues/6
-      //Action performed by disableUniversity() cannot be undone
-      if(universities[university_address].available = false){
-        emit avaliable_status_message(message_immutable);
+       //Already disabled
+      if(!universities[university_address].available){
+          revert("This university has already been disabled.");
       }else{
          universities[university_address].available = false;
-         emit avaliable_status_message(message_unavaliable);
+         emit avaliable_status_message("This University has been disabled for project submissions");
       }
-      
-      
-
     }
     
     function submitProject(bytes32 document_hash, address payable university_address) public payable { // Step 2 and 4
@@ -85,11 +79,16 @@ contract ProjectSubmission {
     require(universities[university_address].isregistered, "Projects can only be submitted to registred universities.");
     //A university must be available to accept project submissions
     require(universities[university_address].available, "Projects can only be submitted to available universities.");
-
-    Project memory project = Project(msg.sender, university_address, ProjectStatus.Waiting, 0);
+    //A project cannot be submitted more than once
+    if(projects[document_hash].issubmitted){
+        revert("This project has already been submitted.");
+    }
+   
+    
+    Project memory project = Project(msg.sender, university_address, ProjectStatus.Waiting, true, 0);
     projects[document_hash] = project;
     ownerBalance += 1 ether;
-    emit submit_project_message(submit_project_success);
+    emit submit_project_message("Project Succesfully submitted!");
     
     }
     
@@ -98,23 +97,29 @@ contract ProjectSubmission {
     }
     
     function reviewProject (bytes32 document_hash, ProjectStatus project_status) public onlyOwner {
+    if (projects[document_hash].status == ProjectStatus.Disabled){
+            revert("This project has been disabled and can no longer be reviewed.");
+        }
     require(projects[document_hash].status == ProjectStatus.Waiting,"A project must be in waiting status in order to be reviewed.");
     if(project_status == ProjectStatus.Approved || project_status == ProjectStatus.Rejected ){
         projects[document_hash].status = ProjectStatus(project_status);
-        emit submit_review_message(submit_review_success);
+        emit submit_review_message("Project Successfully reviewed!");
     }else{
-        emit submit_review_message(submit_review_error);
+        emit submit_review_message("Error submitting Project review!");
     }
     
 
     }
     
     function donate(bytes32 document_hash) public payable { // Step 4
-     require(projects[document_hash].status == ProjectStatus.Approved,"A project must be in approved status in order accept donations.");
+     if (projects[document_hash].status == ProjectStatus.Disabled){
+       revert("This project has been disabled and is no longer accepting donations.");
+   }
+    require(projects[document_hash].status == ProjectStatus.Approved,"A project must be approved in order accept donations. Please review.");
      //70% to the project
      projects[document_hash].balance += msg.value * 7 / 10;
      //20% to the university
-     //universities[projects[document_hash].university].balance = msg.value * 2 / 10;
+     universities[projects[document_hash].university].balance = msg.value * 2 / 10;
     //10% to the university
     ownerBalance += msg.value * 1 / 10;
     }
