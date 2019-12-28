404236bc13287a473cd6c03671d5b1d214798b70
Leon Africa <mr.leon.africa@gmail.com>
2019-12-28 21:46:42 +0200

    Development -> Master (#37)
    * Step 1: #2 #3 #4 #5 #6 (#28)
    
    Step 1 complete
    
    * Step 2: #7 (=> #14) #8 #9 #10 (#29)
    
    Step 2 complete (TODO transfer value student => owner #14 )
    
    * Step 3: #11 #12 #13 (#30)
    
    Step 3 complete
    
    * Step 4: #14 #15 %16 #17 #18 #19 (#31)
    
    * updates (#32)
    
    * Step 5: #20 #21 #22 #23 #24 #25 (#33)
    
    Step 5 complete
    
    * update web3 (#34)
    
    web3 update
    
    * attempt fix #34 (#35)
    
    fix #34 complete
    
    * fix #35 can donate (#36)
    
    fixed #35 - all tests passing - complete

--- contracts/ProjectSubmission.sol
+++ contracts/ProjectSubmission.sol
@@ -1,57 +1,144 @@
-pragma... // Step 1
+pragma solidity ^0.5.0;
 
-contract ProjectSubmission { // Step 1
+contract ProjectSubmission {
 
-    ...owner... // Step 1 (state variable)
-    // ...ownerBalance... // Step 4 (state variable)
-    modifier onlyOwner() { // Step 1
-      ...
+  address payable public owner = msg.sender;
+  uint public ownerBalance;
+
+    event registration_message(string message);
+    event avaliable_status_message(string message);
+    event submit_project_message(string message);
+    event submit_review_message(string message);
+    event withdrawl_success(string message);
+
+
+    modifier onlyOwner(){
+      require(msg.sender == owner, "You are not the owner of this contract!");
+      _;
     }
-    
-    struct University { // Step 1
-        ...available...
-        ...balance...
+
+    struct University {
+        bool available;
+        bool isregistered;
+        uint balance;
     }
-    ...universities... // Step 1 (state variable)
-    
-    // enum ProjectStatus { ... } // Step 2
-    // struct Project { // Step 2
-    //     ...author...
-    //     ...university...
-    //     ...status...
-    //     ...balance...
-    // }
-    // ...projects... // Step 2 (state variable)
-    
-    function registerUniversity... { // Step 1
-      ...
+    mapping(address => University) public universities;
+
+    enum ProjectStatus { Waiting, Rejected, Approved, Disabled }
+
+    struct Project {
+         address payable author;
+         address payable university;
+         ProjectStatus status;
+         bool issubmitted;
+         uint balance;
+     }
+
+    mapping (bytes32 => Project) public projects;
+
+
+    function registerUniversity(address university_address) public onlyOwner{ // Step 1
+
+      University memory university = University(true, true, 0);
+      universities[university_address] = university;
+      emit registration_message("Succesfully Registred!");
+
     }
-    
-    function disableUniversity... { // Step 1
-      ...
+
+    function disableUniversity(address university_address) public onlyOwner {
+       //Already disabled
+      if(!universities[university_address].available){
+          revert("This university has already been disabled.");
+      }else{
+         universities[university_address].available = false;
+         emit avaliable_status_message("This University has been disabled for project submissions");
+      }
     }
-    
-    // function submitProject... { // Step 2 and 4
-    //   ...
-    // }
-    
-    // function disableProject... { // Step 3
-    //   ...
-    // }
-    
-    // function reviewProject... { // Step 3
-    //   ...
-    // }
-    
-    // function donate... { // Step 4
-    //   ...
-    // }
-    
-    // function withdraw... { // Step 5
-    //   ...
-    // }
-    
-    // function withdraw... {  // Step 5 (Overloading Function)
-    //   ...
-    // }
+
+    function submitProject(bytes32 document_hash, address payable university_address) public payable { // Step 2 and 4
+    require(msg.value >= 1 ether, "A value of atleast 1 ether is required to submit a project.");
+    //A university must be registred to accept project submissions
+    require(universities[university_address].isregistered, "Projects can only be submitted to registred universities.");
+    //A university must be available to accept project submissions
+    require(universities[university_address].available, "Projects can only be submitted to available universities.");
+    //A project cannot be submitted more than once
+    if(projects[document_hash].issubmitted){
+        revert("This project has already been submitted.");
+    }
+
+
+    Project memory project = Project(msg.sender, university_address, ProjectStatus.Waiting, true, 0);
+    projects[document_hash] = project;
+    ownerBalance += 1 ether;
+    emit submit_project_message("Project Succesfully submitted!");
+
+    }
+
+    function disableProject (bytes32 document_hash) public onlyOwner{ // Step 3
+    projects[document_hash].status = ProjectStatus.Disabled;
+    }
+
+    function reviewProject (bytes32 document_hash, ProjectStatus project_status) public onlyOwner {
+    if (projects[document_hash].status == ProjectStatus.Disabled){
+            revert("This project has been disabled and can no longer be reviewed.");
+        }
+    require(projects[document_hash].status == ProjectStatus.Waiting,"A project must be in waiting status in order to be reviewed.");
+    if(project_status == ProjectStatus.Approved || project_status == ProjectStatus.Rejected ){
+        projects[document_hash].status = ProjectStatus(project_status);
+        emit submit_review_message("Project Successfully reviewed!");
+    }else{
+        emit submit_review_message("Error submitting Project review!");
+    }
+
+
+    }
+
+    function donate(bytes32 document_hash) public payable { // Step 4
+     if (projects[document_hash].status == ProjectStatus.Disabled){
+       revert("This project has been disabled and is no longer accepting donations.");
+   }
+
+    require(projects[document_hash].status == ProjectStatus.Approved,"A project must be approved in order accept donations. Please review.");
+     //70% to the project
+     projects[document_hash].balance += msg.value * 7 / 10;
+     //20% to the university
+     universities[projects[document_hash].university].balance = msg.value * 2 / 10;
+    //10% to the university
+    ownerBalance += msg.value * 1 / 10;
+    }
+
+
+    //Only Universities and contract owners can withdraw using this function
+    function withdraw() public payable { // Step 5
+
+
+    //The contract owner wants to withdraw
+    if(msg.sender == owner){
+      uint amount = ownerBalance;
+      require(amount > 0, "You have no funds to withdraw");
+      ownerBalance = 0;
+      msg.sender.transfer(amount);
+      emit withdrawl_success("Withdrawl Successful!");
+    }
+
+    //A University wants to withdraw
+    else if(!universities[msg.sender].available || universities[msg.sender].available){
+      uint amount = universities[msg.sender].balance;
+      require(amount > 0, "You have no funds to withdraw");
+      universities[msg.sender].balance = 0;
+      msg.sender.transfer(amount);
+      emit withdrawl_success("Withdrawl Successful!");
+    }else{
+        revert("You are not permitted to use this withdraw function!");
+    }
+
+    }
+
+    //A student wants to withdraw
+    function withdraw(bytes32 document_hash) public payable {  // Step 5 (Overloading Function)
+    uint amount = projects[document_hash].balance;
+    projects[document_hash].balance = 0;
+    msg.sender.transfer(amount);
+    emit withdrawl_success("Withdrawl Successful!");
+    }
 }
